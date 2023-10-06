1619cb002f6b1875204729f0680bcf18906b9f33
VDelCastilloHm <ing.vitidelcastillo@gmail.com>
2023-10-06 18:30:13 -0500

    genres,platform

---
+++ api/src/models/Genres.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('genres', {
+    ID:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {timestamps:false}
+  );
+};
---
+++ api/src/models/Platform.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('platform', {
+    ID:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {timestamps:false}
+  );
+};
