4a568de2fe3b5d934865d24f7322c80a02c017ab
VDelCastilloH <ing.vitidelcastillo@gmail.com>
2023-10-09 21:59:26 -0500

    router/index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,19 @@
 const { Router } = require('express');
 const Server = require('../app');
+const mainRouter = Router();
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+mainRouter.get('/videogames',(req, res)=>{
 
+});
+
+mainRouter.get('/videogames/:idVG', (req,res)=>{
+
+})
+
+mainRouter.get('/videogames')
 
 
 // Configurar los routers
