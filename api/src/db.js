e90c79fba26151c3390263ceac0320d0124bd595
VDelCastilloH <ing.vitidelcastillo@gmail.com>
2023-11-02 22:11:52 -0500

    PI Final Deploy

--- api/src/db.js
+++ api/src/db.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
 const VideogameModel = require('./models/Videogame');
 const GenreModel = require('./models/Genre');
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
-  logging: false, // (true) permite ver en consola las sentencias SQL que Sequelize ejecuta por detras
-  //native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+//   logging: false, // (true) permite ver en consola las sentencias SQL que Sequelize ejecuta por detras
+//   //native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true,
+    }
+  }
 });
 
 // DEFINICION DE MODELOS
